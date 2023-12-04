801466311369c8525b690ffe1dc38960ead967a9
Mohamed Nabil <mamadonabil919@gmail.com>
2023-12-04 06:45:59 +0200

    first

---
+++ client/src/App.tsx
@@ -0,0 +1,32 @@
+import { Box, CssBaseline, ThemeProvider, createTheme } from "@mui/material"
+import { useMemo } from "react"
+import { themeSettings } from "./theme"
+import { BrowserRouter , Routes ,Route} from "react-router-dom"
+import NavBar from "./scenes/navbar"
+import Dashboard from "./scenes/dashboard"
+import Predictions from "./scenes/predictions"
+//import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  const theme = useMemo(()=> createTheme(themeSettings),[])  
+  return (
+
+      <div className="app">
+        <BrowserRouter>
+        <ThemeProvider theme={theme} >
+           <CssBaseline />
+           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <NavBar/>
+            <Routes>
+              <Route path="/" element={<Dashboard/>} />
+              <Route path="/predictions" element={<Predictions/>} />
+            </Routes>
+           </Box>
+        </ThemeProvider>
+        </BrowserRouter>
+      </div>
+    
+  )
+}
+
+export default App
